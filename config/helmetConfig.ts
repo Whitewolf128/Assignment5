1277846ef1bf84064622ea050a70e2c6b12e6dec
Zachary Ross <zross@academic.rrc.ca>
2026-04-02 10:56:46 -0500

    added cors config and cleaned up helmet

--- config/helmetConfig.ts
+++ config/helmetConfig.ts
@@ -3,34 +3,23 @@ import app from "../src/app";
 // Adding custom security headers beyond Helmet defaults
 app.use(helmet());
 
-// Disable the Content-Security-Policy and X-Download-Options headers
-app.use(
-  helmet({
-    contentSecurityPolicy: false,
-    xDownloadOptions: false,
-  }),
-);
-
 // Sets "Content-Security-Policy: default-src 'self';
 // script-src 'self' example.com;object-src 'none';
 // upgrade-insecure-requests"
+// Disable the Content-Security-Policy and X-Download-Options headers
 app.use(
   helmet({
     contentSecurityPolicy: {
-      useDefaults: false,
-      directives: {
-        defaultSrc: ["'self'"],
-        scriptSrc: ["'self'", "example.com"],
-        objectSrc: ["'none'"],
-        upgradeInsecureRequests: [],
-      },
+        useDefaults: false,
+        directives: {
+            defaultSrc: ["'self'"],
+            scriptSrc: ["'self'", "example.com"],
+            objectSrc: ["'none'"],
+            upgradeInsecureRequests: [],
+        },
     },
-  }),
-);
-
-app.use(
-  helmet({
     xDownloadOptions: false,
     xFrameOptions: false,
   }),
 );
+
