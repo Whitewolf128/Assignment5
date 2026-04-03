9516fb0dc42f57f76db164c15543e75d3ab8e62e
Zachary Ross <zross@academic.rrc.ca>
2026-04-03 00:35:46 -0500

    changed the validation so the server can run

--- src/api/v1/validation/eventValidation.ts
+++ src/api/v1/validation/eventValidation.ts
@@ -10,7 +10,7 @@ export const postSchemas = {
             name: Joi.string().required().min(3).max(80),
             capacity: Joi.number().required().integer().min(5).positive(),
             registrationCount: Joi.number().required().integer().max(100).positive().precision(2),
-            date: Joi.date().format("yyyy-mm-dd hh:mm:ss").required().greater('now'),
+            date: Joi.date().required().greater('now'),
             status:  Joi.string().required().valid("active", "cancelled", "completed"),
             category:  Joi.string().required().valid("confrence", "workshop", "meetup", "seminar", "general"),
             content: Joi.string().required().messages({
@@ -26,7 +26,7 @@ export const postSchemas = {
             name: Joi.string().required().min(3).max(80),
             capacity: Joi.number().required().integer().min(5).positive(),
             registrationCount: Joi.number().required().integer().max(100).positive().precision(2),
-            date: Joi.date().format("yyyy-mm-dd hh:mm:ss").required().greater('now'),
+            date: Joi.date().required().greater('now'),
             status:  Joi.string().required().valid("active", "cancelled", "completed"),
             category:  Joi.string().required().valid("confrence", "workshop", "meetup", "seminar", "general"),
     },
